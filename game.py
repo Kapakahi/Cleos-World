3ef9a54ba8878f9a785b795d79a4ca2c314ddd4c
Kapakahi <rubbishman@gmail.com>
2020-05-27 01:52:07 +0900

    got jump animation working

--- game.py
+++ game.py
@@ -11,48 +11,89 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Cleo's World")
 
 filepath = os.path.dirname(__file__)
-background_image = pygame.image.load(os.path.join(filepath, "practicebk.png")).convert()
+background_image = pygame.image.load(os.path.join(filepath, "images/practicebk.png")).convert()
 
-R1 = pygame.image.load(os.path.join(filepath, "R1.png")).convert()
-R2 = pygame.image.load(os.path.join(filepath, "R2.png")).convert()
-R3 = pygame.image.load(os.path.join(filepath, "R3.png")).convert()
-R4 = pygame.image.load(os.path.join(filepath, "R4.png")).convert()
-R5 = pygame.image.load(os.path.join(filepath, "R5.png")).convert()
-R6 = pygame.image.load(os.path.join(filepath, "R6.png")).convert()
-R7 = pygame.image.load(os.path.join(filepath, "R7.png")).convert()
-R8 = pygame.image.load(os.path.join(filepath, "R8.png")).convert()
-R9 = pygame.image.load(os.path.join(filepath, "R9.png")).convert()
+width = 100
+height = 100
 
-L1 = pygame.image.load(os.path.join(filepath, "L1.png")).convert()
-L2 = pygame.image.load(os.path.join(filepath, "L2.png")).convert()
-L3 = pygame.image.load(os.path.join(filepath, "L3.png")).convert()
-L4 = pygame.image.load(os.path.join(filepath, "L4.png")).convert()
-L5 = pygame.image.load(os.path.join(filepath, "L5.png")).convert()
-L6 = pygame.image.load(os.path.join(filepath, "L6.png")).convert()
-L7 = pygame.image.load(os.path.join(filepath, "L7.png")).convert()
-L8 = pygame.image.load(os.path.join(filepath, "L8.png")).convert()
-L9 = pygame.image.load(os.path.join(filepath, "L9.png")).convert()
+R1 = pygame.image.load(os.path.join(filepath, "images/Walk (1).png"))
+R1 = pygame.transform.scale(R1, (width, height))
+R2 = pygame.image.load(os.path.join(filepath, "images/Walk (2).png"))
+R2 = pygame.transform.scale(R2, (width, height))
+R3 = pygame.image.load(os.path.join(filepath, "images/Walk (3).png"))
+R3 = pygame.transform.scale(R3, (width, height))
+R4 = pygame.image.load(os.path.join(filepath, "images/Walk (4).png"))
+R4 = pygame.transform.scale(R4, (width, height))
+R5 = pygame.image.load(os.path.join(filepath, "images/Walk (5).png"))
+R5 = pygame.transform.scale(R5, (width, height))
+R6 = pygame.image.load(os.path.join(filepath, "images/Walk (6).png"))
+R6 = pygame.transform.scale(R6, (width, height))
+R7 = pygame.image.load(os.path.join(filepath, "images/Walk (7).png"))
+R7 = pygame.transform.scale(R7, (width, height))
+R8 = pygame.image.load(os.path.join(filepath, "images/Walk (8).png"))
+R8 = pygame.transform.scale(R8, (width, height))
+R9 = pygame.image.load(os.path.join(filepath, "images/Walk (9).png"))
+R9 = pygame.transform.scale(R9, (width, height))
+R10 = pygame.image.load(os.path.join(filepath, "images/Walk (10).png"))
+R10 = pygame.transform.scale(R10, (width, height))
 
-char = pygame.image.load(os.path.join(filepath, "standing.png")).convert()
+L1 = pygame.image.load(os.path.join(filepath, "images/WalkLeft (1).png"))
+L1 = pygame.transform.scale(L1, (width, height))
+L2 = pygame.image.load(os.path.join(filepath, "images/WalkLeft (2).png"))
+L2 = pygame.transform.scale(L2, (width, height))
+L3 = pygame.image.load(os.path.join(filepath, "images/WalkLeft (3).png"))
+L3 = pygame.transform.scale(L3, (width, height))
+L4 = pygame.image.load(os.path.join(filepath, "images/WalkLeft (4).png"))
+L4 = pygame.transform.scale(L4, (width, height))
+L5 = pygame.image.load(os.path.join(filepath, "images/WalkLeft (5).png"))
+L5 = pygame.transform.scale(L5, (width, height))
+L6 = pygame.image.load(os.path.join(filepath, "images/WalkLeft (6).png"))
+L6 = pygame.transform.scale(L6, (width, height))
+L7 = pygame.image.load(os.path.join(filepath, "images/WalkLeft (7).png"))
+L7 = pygame.transform.scale(L7, (width, height))
+L8 = pygame.image.load(os.path.join(filepath, "images/WalkLeft (8).png"))
+L8 = pygame.transform.scale(L8, (width, height))
+L9 = pygame.image.load(os.path.join(filepath, "images/WalkLeft (9).png"))
+L9 = pygame.transform.scale(L9, (width, height))
+L10 = pygame.image.load(os.path.join(filepath, "images/WalkLeft (10).png"))
+L10 = pygame.transform.scale(L10, (width, height))
 
+J1 = pygame.image.load(os.path.join(filepath, "images/Jump (1).png"))
+J1 = pygame.transform.scale(J1, (width, height))
+J2 = pygame.image.load(os.path.join(filepath, "images/Jump (2).png"))
+J2 = pygame.transform.scale(J2, (width, height))
+J3 = pygame.image.load(os.path.join(filepath, "images/Jump (3).png"))
+J3 = pygame.transform.scale(J3, (width, height))
+J4 = pygame.image.load(os.path.join(filepath, "images/Jump (4).png"))
+J4 = pygame.transform.scale(J4, (width, height))
+J5 = pygame.image.load(os.path.join(filepath, "images/Jump (5).png"))
+J5 = pygame.transform.scale(J5, (width, height))
+J6 = pygame.image.load(os.path.join(filepath, "images/Jump (6).png"))
+J6 = pygame.transform.scale(J6, (width, height))
+J7 = pygame.image.load(os.path.join(filepath, "images/Jump (7).png"))
+J7 = pygame.transform.scale(J7, (width, height))
+J8 = pygame.image.load(os.path.join(filepath, "images/Jump (8).png"))
+J8 = pygame.transform.scale(J8, (width, height))
+J9 = pygame.image.load(os.path.join(filepath, "images/Jump (9).png"))
+J9 = pygame.transform.scale(J9, (width, height))
+J10 = pygame.image.load(os.path.join(filepath, "images/Jump (10).png"))
+J10 = pygame.transform.scale(J10, (width, height))
 
+char = pygame.image.load(os.path.join(filepath, "images/Idle (1).png"))
+char = pygame.transform.scale(char, (width, height))
 
-
-favicon = pygame.image.load(os.path.join(filepath, "cat-fav.png"))
-
+favicon = pygame.image.load(os.path.join(filepath, "images/
+cat-fav.png"))
 pygame.display.set_icon(favicon)
 
-walkRight = [R1, R2, R3, R4, R5, R6, R7, R8, R9]
-walkLeft = [L1, L2, L3, L4, L5, L7, L7, L8, L9]
-#bg = pygame.image.load('background_image')
-#char = pygame.image.load('standing')
-
+walkRight = [R1, R2, R3, R4, R5, R6, R7, R8, R9, R10]
+walkLeft = [L1, L2, L3, L4, L5, L7, L7, L8, L9, L10]
+jumpUp = [J1,J1,J2,J2,J3,J3,J4 ,J4,J5,J5,J6,J6,J7,J7,J8,J8,J9,J9,J10,J10]
+#jumpUp = [J1,J1,J1,J2,J2,J2,J3,J3,J3,J4,J4 J4,J5,J5,J5,J6,J6,J6,J7,J7,J7,J8,J8,J8,J9,J9,J9,J10,J10,J10]
 clock = pygame.time.Clock()
 
 x = 50
 y = 450
-width = 64 #change this with different sprite
-height = 64
 vel = 5
 isJump = False
 jumpCount = 10
@@ -62,16 +103,23 @@ walkCount = 0
 
 def redrawGameWindow():
     global walkCount
+    global jumpCount
     screen.blit(background_image, (0,0))
 
-    if walkCount + 1 >= 27:
+    if walkCount + 1 >= 30:
         walkCount = 0
-    if left:
+    elif left:
         screen.blit(walkLeft[walkCount//3], (x,y))
         walkCount += 1
     elif right:
         screen.blit(walkRight[walkCount//3], (x,y))
         walkCount +=1
+    elif isJump:
+        for i in jumpUp:
+            screen.blit(i, (x, y))
+        #     jumpCount +=1
+        # screen.blit(jumpUp[(jumpCount + 10)//3], (x,y))
+        # jumpCount +=1    
     else:
         screen.blit(char, (x,y))    
     pygame.display.update()
@@ -79,11 +127,9 @@ def redrawGameWindow():
 
 
 #Loop to keep displaying the window
-running = True
-#How fast the screen reloads
-#clock = pygame.time.Clock()
+running = True 
 while running:
-    clock.tick(27)
+    clock.tick(30)
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
             running = False
@@ -117,7 +163,7 @@ while running:
             neg = 1
             if jumpCount < 0:
                 neg = -1
-            y -= (jumpCount ** 2) * 0.5 * neg
+            y -= int((jumpCount **2) * 0.5 * neg)
             jumpCount -= 1
         else:
             isJump = False
